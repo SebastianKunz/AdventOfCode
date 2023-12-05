e70d521a0c4a2034349cb1278047c953175bf05f
Sebastian Kunz <kunz.seb@outlook.de>
2023-12-06 00:04:48 +0100

    Day 05 takes ~ 1 min now

--- 2023/AOC.2023.Day05/Almanac.cs
+++ 2023/AOC.2023.Day05/Almanac.cs
@@ -2,10 +2,6 @@
 
 public class Almanac
 {
-    private readonly Dictionary<string, List<DestinationSourceRange>> _allMaps;
-
-    private readonly Dictionary<string, DestinationSourceRange?> _rangeCache = new();
-
     public Almanac(List<SeedPair> seeds, Dictionary<string, List<DestinationSourceRange>> allMaps)
     {
         foreach (List<DestinationSourceRange> val in allMaps.Values)
@@ -13,47 +9,36 @@ public class Almanac
             val.Sort((a, b) => a.SourceRangeStart.CompareTo(b.SourceRangeStart));
         }
 
-        _rangeCache = new Dictionary<string, DestinationSourceRange?>();
-        _rangeCache.Add("seed-to-soil", null);
-        _rangeCache.Add("soil-to-fertilizer", null);
-        _rangeCache.Add("fertilizer-to-water", null);
-        _rangeCache.Add("water-to-light", null);
-        _rangeCache.Add("light-to-temperature", null);
-        _rangeCache.Add("temperature-to-humidity", null);
-        _rangeCache.Add("humidity-to-location", null);
-
-        _allMaps = allMaps;
+        _seedToSoil = allMaps["seed-to-soil"];
+        _soilToFertilizer = allMaps["soil-to-fertilizer"];
+        _fertilizerToWater = allMaps["fertilizer-to-water"];
+        _waterToLight = allMaps["water-to-light"];
+        _lightToTemperature = allMaps["light-to-temperature"];
+        _temperatureToHumidity = allMaps["temperature-to-humidity"];
+        _humidityToLocation = allMaps["humidity-to-location"];
 
         Seeds = seeds;
     }
 
     private List<SeedPair> Seeds { get; }
 
-    private List<DestinationSourceRange> SeedToSoil => _allMaps["seed-to-soil"];
-    private List<DestinationSourceRange> SoilToFertilizer => _allMaps["soil-to-fertilizer"];
-    private List<DestinationSourceRange> FertilizerToWater => _allMaps["fertilizer-to-water"];
-    private List<DestinationSourceRange> WaterToLight => _allMaps["water-to-light"];
-    private List<DestinationSourceRange> LightToTemperature => _allMaps["light-to-temperature"];
-    private List<DestinationSourceRange> TemperatureToHumidity => _allMaps["temperature-to-humidity"];
-    private List<DestinationSourceRange> HumidityToLocation => _allMaps["humidity-to-location"];
+    private readonly List<DestinationSourceRange> _seedToSoil;
+    private readonly List<DestinationSourceRange> _soilToFertilizer;
+    private readonly List<DestinationSourceRange> _fertilizerToWater;
+    private readonly List<DestinationSourceRange> _waterToLight;
+    private readonly List<DestinationSourceRange> _lightToTemperature;
+    private readonly List<DestinationSourceRange> _temperatureToHumidity;
+    private readonly List<DestinationSourceRange> _humidityToLocation;
 
-    public ulong FindSource(ulong number, string mode, List<DestinationSourceRange> map)
+    private ulong FindSource(ulong number, List<DestinationSourceRange> map)
     {
-        var rangeHint = _rangeCache[mode];
-        var cachedRange = CalcDestination(number, rangeHint);
-        if (cachedRange.HasValue)
-        {
-            return cachedRange.Value;
-        }
-
         DestinationSourceRange? range = FindClosestRange(number, map);
-        _rangeCache[mode] = range;
 
-        cachedRange = CalcDestination(number, range);
+        var mapping = CalcDestination(number, range);
 
-        if (cachedRange.HasValue)
+        if (mapping.HasValue)
         {
-            return cachedRange.Value;
+            return mapping.Value;
         }
 
         return number;
@@ -103,15 +88,15 @@ public class Almanac
         return map[resultIndex];
     }
 
-    public ulong CalculateLocationFor(ulong seedNumber)
+    private ulong CalculateLocationFor(ulong seedNumber)
     {
-        ulong soilNumber = FindSource(seedNumber, "seed-to-soil", SeedToSoil);
-        ulong fertilizerNumber = FindSource(soilNumber,"soil-to-fertilizer", SoilToFertilizer);
-        ulong waterNumber = FindSource(fertilizerNumber,"fertilizer-to-water", FertilizerToWater);
-        ulong lightNumber = FindSource(waterNumber,"water-to-light", WaterToLight);
-        ulong tempNumber = FindSource(lightNumber,"light-to-temperature", LightToTemperature);
-        ulong humidityNumber = FindSource(tempNumber,"temperature-to-humidity", TemperatureToHumidity);
-        ulong locationNumber = FindSource(humidityNumber, "humidity-to-location", HumidityToLocation);
+        ulong soilNumber = FindSource(seedNumber, _seedToSoil);
+        ulong fertilizerNumber = FindSource(soilNumber, _soilToFertilizer);
+        ulong waterNumber = FindSource(fertilizerNumber, _fertilizerToWater);
+        ulong lightNumber = FindSource(waterNumber, _waterToLight);
+        ulong tempNumber = FindSource(lightNumber, _lightToTemperature);
+        ulong humidityNumber = FindSource(tempNumber, _temperatureToHumidity);
+        ulong locationNumber = FindSource(humidityNumber, _humidityToLocation);
         return locationNumber;
     }
 
